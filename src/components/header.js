96e6b7ec6cc3ae5ca87daf345b96c1fcdd4711bc
Dylan Britz <47594494+britzdylan@users.noreply.github.com>
2020-03-01 07:42:13 +0200

    added mobile & desktop navigatio, made header responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import headerStyles from './header.module.scss'
+import headerStyles from './header.module.css'
 
 
 
-const Header = () => {
+const Header = () => { //main home page header 
     return (
-        <header>
-            <div className={headerStyles.topHeader} >
-                <small><a href="mailto:info@netwater.co.za" className="">info@netwater.co.za</a></small>
-                <small><a href="tel:013261614" className="">011 326 1614</a></small>
+        <header> 
+
+            {/* top bar with contact information */}
+            <div className={headerStyles.top} > 
+                <small><a href="mailto:info@netwater.co.za" className={headerStyles.email}>info@netwater.co.za</a></small>
+                <small><a href="tel:013261614" id="cell" className={headerStyles.cell}>011 326 1614</a></small>
             </div>
-            <nav className={headerStyles.nav-bar} >
-                <h2 className="">bramFischer</h2>
-                <ul className="main-menu">
+
+            {/* main navigation for desktops & tablets */}
+            <nav className={headerStyles.nav} >
+                <h1 className={headerStyles.brand}><Link to="/">bramFischer</Link></h1>
+                <ul className={headerStyles.mainMenu}>
                     <li>
                         <Link to="/stores" >Store Directory</Link>
                     </li>
@@ -24,11 +28,37 @@ const Header = () => {
                         <Link to="/lease" >Lease from us</Link>
                     </li>
                 </ul>
-                </nav>
-            <div className={headerStyles.page-header} >
+            </nav>
+
+            {/* Page header */}
+
+            <div className={headerStyles.header} >
                 <h1 className="wht">Hello Randburg, Welcome to Bram Fischer Centre, Your one stop convenient shopping experience.</h1>
                 <Link to="/stores" className="btn btn-alt">Browse our Stores</Link>
             </div>
+
+
+            {/* bottom mobile navigation */}
+            <nav className={headerStyles.bottom}>
+                <ul className={headerStyles.mobileMenu}>
+                    <li>
+                        
+                        <Link to="/stores" ><img src="../../cart.svg" alt="" className={headerStyles.mobileIcon} height="25px" width="25px"/>Stores</Link>
+                    </li>
+                    <li>
+                        
+                        <Link to="/contact-us" ><img src="../../atm.svg" alt="" className={headerStyles.mobileIcon} height="25px" width="25px"/>Contact</Link>
+                    </li>
+                    <li>
+                        
+                        <Link to="/lease" ><img src="../../parking.svg" alt="" className={headerStyles.mobileIcon} height="25px" width="25px" />Lease</Link>
+                    </li>
+                    <li>
+                        
+                        <Link to="" ><div className={headerStyles.circle}></div><div className={headerStyles.circle}></div><div className={headerStyles.circle}></div></Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
