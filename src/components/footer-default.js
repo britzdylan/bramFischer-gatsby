c99b0078ef70c05130ffdb30df2c1a7e6ff7e9b2
Dylan Britz <britzdylan@gmail.com>
2020-03-11 12:12:19 +0200

    fixed maps

--- src/components/footer-default.js
+++ src/components/footer-default.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import React from "react"
 import footerStyles from "../components/footer.module.css"
-import GoogleMap from "../components/maps.jsx"
+import 'lazysizes' 
 
 // query StaticMapQuery `{
 //     staticMap {
@@ -38,7 +38,13 @@ const Footer = () => {
                 </div>
                 
                 <div className={footerStyles.map} id="map">
-                    <GoogleMap />
+                    <a href="https://www.google.co.za/maps/place/Bram+Fischer+Shopping+Centre/@-26.1112008,27.9991991,17z/data=!4m5!3m4!1s0x1e95754c62f41317:0x19f3e39ae4a9722!8m2!3d-26.11109!4d28.00039" target="_blank" rel="noopener noreferrer">
+                    <picture>
+                        <source data-srcset="../../map.webp" type="image/webp"/>
+                        <source data-srcset="../../map.jpg" type="image/jpg" className="lazyload"/> 
+                        <img data-src="../../map.jpg" alt="bramFischer location" className="lazyload"/>
+                   </picture>
+                    </a>
                 </div>
                 </div>
             {/* copyright info and extra links */}
